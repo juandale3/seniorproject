426bedd46ea47bc3d318d016614c7174574687a6
Tjaber02 <140358431+Tjaber02@users.noreply.github.com>
2023-11-10 01:14:40 -0800

    Create Solenoid_and_Steppermotor_code.ino
    this is the code for both the solenoid and the stepper motor all in one code file.

---
+++ Solenoid_and_Steppermotor_code/Solenoid_and_Steppermotor_code.ino
@@ -0,0 +1,61 @@
+#include <AccelStepper.h>
+
+// Define the connections for solenoid valves
+const int openSolenoidPin = 4;  // Digital pin for opening solenoid valve
+const int closeSolenoidPin = 5; // Digital pin for closing solenoid valve
+
+// Define the connections for the stepper motor
+#define PUL_PIN 2  // Connect PUL+ to Arduino digital pin 2
+#define DIR_PIN 3  // Connect DIR+ to Arduino digital pin 3
+#define DIR_INVERT true  // Set to 'true' if the motor direction needs to be inverted
+
+// Create an AccelStepper object
+AccelStepper stepper(AccelStepper::DRIVER, PUL_PIN, DIR_PIN);
+
+void setup() {
+  // Set the solenoid valve pins as outputs
+  pinMode(openSolenoidPin, OUTPUT);
+  pinMode(closeSolenoidPin, OUTPUT);
+
+  // Set the maximum speed and acceleration for the stepper motor
+  stepper.setMaxSpeed(1000);  // Adjust this value as needed
+  stepper.setAcceleration(500); // Adjust this value as needed
+
+  // Set the direction inversion if needed
+  stepper.setPinsInverted(DIR_INVERT, false, false);
+
+  // Set the initial position (optional)
+  stepper.setCurrentPosition(0);
+
+  // You can also set the motor's enable pin (ENBL) if needed
+  // pinMode(ENBL_PIN, OUTPUT);
+  // digitalWrite(ENBL_PIN, LOW); // Enable the motor
+}
+
+void loop() {
+  // Open the solenoid valve
+  digitalWrite(openSolenoidPin, LOW);
+  digitalWrite(closeSolenoidPin, HIGH);
+  delay(3000);  // Open the valve for 3 seconds
+
+  // Close the solenoid valve
+  digitalWrite(openSolenoidPin, HIGH);
+  digitalWrite(closeSolenoidPin, LOW);
+  delay(3000);  // Close the valve for 3 seconds
+
+  // Move the motor a specified number of steps
+  stepper.moveTo(905);  // Adjust the number of steps as needed
+
+  // Run the motor until it reaches the target position
+  while (stepper.isRunning()) {
+    stepper.run();
+  }
+
+  // Reverse the direction and move back to the starting position
+  stepper.moveTo(0);
+  while (stepper.isRunning()) {
+    stepper.run();
+  }
+
+  // You can repeat this loop or implement your desired logic here
+}
